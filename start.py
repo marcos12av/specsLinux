96ce102f915c6ae5830ff105ca5604f1cb13f57c
MarcosMVK <marcos12av@gmail.com>
2024-03-11 23:16:23 -0600

    two

--- start.py
+++ start.py
@@ -1,35 +1,42 @@
 import subprocess
 
-def getHostnameData(keyword):
-    outcommand = subprocess.run("hostnamectl", stdout=subprocess.PIPE).stdout.decode("utf-8")
-    outcommandlines = outcommand.split('\n')
-    linekeyword = ""
-    for line in outcommandlines:
-        if keyword in line:
-            linekeyword = line
-            hardwareVendor = linekeyword.split(":")
-            hardwareVendor = hardwareVendor[1].strip()
-            return hardwareVendor
-            break
-        else:
-            hardwareVendor = " "
-            return hardwareVendor
+class linuxSpec:
+
+    def getOS(self):
+        lineTextOS = (subprocess.run(["lsb_release", "-d"], stdout=subprocess.PIPE).stdout.decode("utf-8"))
+        splitLineTextOS = lineTextOS.split()
+        os = " ".join(splitLineTextOS[1:])
+        return os
     
+    def getHostnameData(self, keyword):
+        hostnamectlText = self.shellCMD("hostnamectl")
+        hostnamectlTextLines = hostnamectlText.split('\n')
+        for line in hostnamectlTextLines:
+            if keyword in line:
+                hardwareVendor = line.split(":")
+                return hardwareVendor[1].strip()
+            else:
+                hardwareVendor = " "
+                return hardwareVendor
+    
+    def shellCMD(self, shellCMD):
+        outcommand = subprocess.run(["hostname", "-I"], stdout=subprocess.PIPE).stdout.decode("utf-8")
+        return outcommand
+    
+systemOS = linuxSpec()
 
 fileSpecs = open("specs.txt", "a")
 fileSpecs.write("Características Generales\n")
 fileSpecs.write("Nombre de host:\t\t\t\t\t\t\t\t")
-fileSpecs.write(subprocess.run(["hostname"], stdout=subprocess.PIPE).stdout.decode("utf-8"))
+fileSpecs.write(systemOS.shellCMD("hostname"))
 fileSpecs.write("Versión del sistema operativo:\t\t\t\t")
-sentence = (subprocess.run(["lsb_release", "-d"], stdout=subprocess.PIPE).stdout.decode("utf-8"))
-palabras = sentence.split()
-os = ""
-for x in palabras[1:]:
-    os += x + " "
-os = os[:-1]
-fileSpecs.write(os + "\n")
+fileSpecs.write(systemOS.getOS() + "\n")
 fileSpecs.write("Dirección IP:\t\t\t\t\t\t\t\t")
-fileSpecs.write(subprocess.run(["hostname", "-I"], stdout=subprocess.PIPE).stdout.decode("utf-8"))
+cmd = "\"hostname\", \"-I\""
+print(cmd)
+print("\n")
+fileSpecs.write(systemOS.shellCMD(cmd))
+
 fileSpecs.write("Versión base del kernel:\t\t\t\t\t")
 fileSpecs.write(subprocess.run(["uname", "-sr"], stdout=subprocess.PIPE).stdout.decode("utf-8"))
 fileSpecs.write("Versión del kernel:\t\t\t\t\t\t\t")
@@ -51,44 +58,37 @@ fileSpecs.write("Procesador(es) virtual(es):\t\t\t\t\t")
 fileSpecs.write(subprocess.run(["nproc"], stdout=subprocess.PIPE).stdout.decode("utf-8"))
 fileSpecs.write("Versión del BIOS:\t\t\t\t\t\t\t")
 fileSpecs.write(subprocess.run(["dmidecode", "-s", "bios-version"], stdout=subprocess.PIPE).stdout.decode("utf-8"))
-
+#### Salto para pruebas
+fileSpecs.write("\n")
 # Get disks number and unids label
-textLineDisk = []
-incremental = 0
 
-textLineOsDisk = []
-textLinePartitionCache = []
+def diskInfo (diskProperty, diskText):
+    textLineDisk = []
+    findText = None
 
-outcommand = subprocess.run("lsblk", stdout=subprocess.PIPE).stdout.decode("utf-8")
-outcommandlines = outcommand.split('\n')
-valid = None
-testDiskCache = ""
-textDiskSO = ""
-for linedisk in outcommandlines:
-        if "disk" in linedisk:
-            diskIndice = outcommandlines.index(linedisk) + 1
-            diskContentList = []
-            diskContentList.append(linedisk)
-            for linesubdisk in outcommandlines[diskIndice:]:
-                if "disk" in linesubdisk:
-                    break
-                else:
-                    diskContentList.append(linesubdisk)
-            textLineDisk.append(diskContentList)
+    # lsblk detect disks and save disks and partitions in arrays
+    outcommand = subprocess.run("lsblk", stdout=subprocess.PIPE).stdout.decode("utf-8")
+    outcommandlines = outcommand.split('\n')
+    for linedisk in outcommandlines:
+            if "disk" in linedisk:
+                diskIndice = outcommandlines.index(linedisk) + 1
+                diskContentList = []
+                diskContentList.append(linedisk)
+                for linesubdisk in outcommandlines[diskIndice:]:
+                    if "disk" in linesubdisk:
+                        break
+                    else:
+                        diskContentList.append(linesubdisk)
+                textLineDisk.append(diskContentList)
 
-for disk in textLineDisk:
-    print(disk)           
-"""        if valid:
-            if "boot" in line:
-                lineWordsplit = linekeyword[incremental-1].split(" ")           
-                textDiskSO = "/dev/" + lineWordsplit[0] + " para so"
-                valid = False
-            if "/var/cache/ecarto" in line:
-                lineWordsplit = linekeyword[incremental-1].split(" ")
-                testDiskCache = "/dev/" + lineWordsplit[0] + " para caché de mapas"
-                valid = False
-        #Buscar discos en las particiones 
-"""        
+    # Find disk property and return then
+    for disk in textLineDisk:
+        for diskSection in disk:
+            if diskProperty in diskSection:
+                diskSectionSplitText = disk[0].split(" ")
+                diskPath = "/dev/" + diskSectionSplitText[0] + diskText
+                return diskPath
+     
 textDisk = str(len(textLineDisk)) + ' unidades\n'
 
 fileSpecs.write("Discos duros virtuales:\t\t\t\t\t\t")
@@ -96,9 +96,12 @@ fileSpecs.write(textDisk)
 fileSpecs.write("Ubicación de discos en host:\t\t\t\t\n")
 fileSpecs.write("Unidades lógicas de almacenamiento:\t\t\t")
 fileSpecs.write(textDisk)
-textDiskSO = "\t\t\t\t\t\t\t\t\t\t\t" + textDiskSO + "\n"
+ 
+
+textDiskSO = "\t\t\t\t\t\t\t\t\t\t\t" + diskInfo("boot", " para SO") + "\n"
 fileSpecs.write(textDiskSO)
-testDiskCache = "\t\t\t\t\t\t\t\t\t\t\t" + testDiskCache + "\n"
+
+testDiskCache = "\t\t\t\t\t\t\t\t\t\t\t" + diskInfo("/var/cache/ecarto", " para caché de mapas") + "\n"
 fileSpecs.write(testDiskCache)
 fileSpecs.write("Particionado del sistema (LVM):\t\t\t")
 
